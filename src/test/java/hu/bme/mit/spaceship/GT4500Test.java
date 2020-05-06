2833a8d8759b945b0a0756239c7dea14570c8d09
= <mdani278@gmail.com>
2020-05-06 23:35:11 +0200

    Initial tests implemented

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,45 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore ts1;
+  private TorpedoStore ts2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    ts1 = mock(TorpedoStore.class);
+    ts2 = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(ts1, ts2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(ts1.fire(anyInt())).thenReturn(true);
+    when(ts2.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(ts1, times(1)).fire(anyInt());
+    verify(ts2, times(0)).fire(anyInt());
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(ts1.fire(anyInt())).thenReturn(true);
+    when(ts2.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(ts1, times(1)).fire(anyInt());
+    verify(ts2, times(1)).fire(anyInt());
   }
 
 }
