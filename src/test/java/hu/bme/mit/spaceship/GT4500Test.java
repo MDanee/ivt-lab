b5123b13aaf2df670f5dc5b7960ac1baea8bcba8
= <mdani278@gmail.com>
2020-05-07 00:47:07 +0200

    Code coverage done

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -145,4 +145,72 @@ public class GT4500Test {
     verify(ts1, times(0)).fire(anyInt());
     verify(ts2, times(1)).fire(anyInt());
   }
+
+  @Test
+  public void FillerTest(){
+
+    // Act
+    GT4500 ship = new GT4500();
+    boolean laser = ship.fireLaser(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, laser);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_Secondary(){
+    // Arrange
+    when(ts1.fire(anyInt())).thenReturn(false);
+    when(ts2.fire(anyInt())).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(ts1.isEmpty()).thenReturn(true);
+    when(ts2.isEmpty()).thenReturn(false);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(ts1, times(1)).fire(anyInt());
+    verify(ts2, times(2)).fire(anyInt());
+  }
+
+
+  @Test
+  public void fireTorpedo_Single_Success_Secondary_Empty(){
+    // Arrange
+    when(ts1.fire(anyInt())).thenReturn(true);
+    when(ts2.fire(anyInt())).thenReturn(false);
+    when(ts1.isEmpty()).thenReturn(false);
+    when(ts2.isEmpty()).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(ts1, times(3)).fire(anyInt());
+    verify(ts2, times(0)).fire(anyInt());
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_Primary_Empty(){
+    // Arrange
+    when(ts1.fire(anyInt())).thenReturn(false);
+    when(ts2.fire(anyInt())).thenReturn(true);
+    when(ts1.isEmpty()).thenReturn(true);
+    when(ts2.isEmpty()).thenReturn(false);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(ts1, times(0)).fire(anyInt());
+    verify(ts2, times(2)).fire(anyInt());
+  }
 }
